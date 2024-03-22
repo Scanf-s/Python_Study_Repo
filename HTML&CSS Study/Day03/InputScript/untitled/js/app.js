7de1b68ddd476543da35913be61e3ec5615ae812
Scanf-s <calzone0404@gmail.com>
2024-03-22 18:13:45 +0900

    20240322 - Javascript
    Challenge 1

---
+++ HTML&CSS Study/Day03/InputScript/untitled/js/app.js
@@ -0,0 +1,7 @@
+const nameElement = document.getElementById('name');
+const animalElement = document.getElementById('animal');
+const fruitElement = document.getElementById('fruit');
+
+nameElement.textContent  = prompt('What is your name?');
+animalElement.textContent = prompt('What is your favorite animal?');
+fruitElement.textContent  = prompt('What is your favorite fruit?');

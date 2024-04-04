e1877655d99bd23236e57f557fd29250293d2dd2
YANG UI JONG <calzone0404@gmail.com>
2024-04-04 17:56:12 +0900

    hangman!

--- [Study] Python/asdf.py
+++ [Study] Python/asdf.py
@@ -1,8 +1,2 @@
-def print_lengths(strArr) -> list:
-    strlen = list()
-    for str in strArr:
-        strlen.append(f'{str}의 길이 : {len(str)}')
-    return strlen
-
-random_str = ["asdfoighja", "kihgfisfjg", "fjgio", "dijhfoasjhdiufja", "lkvnblsdijkfgb", "jfhngjk"]
-print_lengths(random_str)
+n = int(input())
+print("수" if n % 2 == 0 else "박" for i in range(n))

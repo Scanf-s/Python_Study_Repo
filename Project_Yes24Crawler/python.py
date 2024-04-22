c230c40f53464020d710035b3301ceb39e21eb2f
Scanf-s <calzone0404@gmail.com>
2024-04-22 18:31:33 +0900

    YAY

--- Project_Yes24Crawler/python.py
+++ Project_Yes24Crawler/python.py
@@ -1,58 +1,171 @@
-# webdriver-manager
-from selenium.common import NoSuchElementException
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-# selenium
+import datetime
+from datetime import datetime
+
 from selenium import webdriver
-
-ChromeDriverManager().install()
-
-browser = webdriver.Chrome()
-# url = 'https://www.yes24.com/Product/category/bestseller?CategoryNumber=001&sumgb=06'
-# browser.get(url)
-
-# 1페이지의 링크 데이터 전부 수집
-
-# 1개의 링크 수집
-# print(browser.find_element(By.CLASS_NAME, 'gd_name').get_attribute('href'))
-
-# 1페이지 전체 링크 데이터 수집
-# datas = browser.find_elements(By.CLASS_NAME, 'gd_name')
-# for data in datas:
-#     print(data.get_attribute('href'))
-
-# n 페이지 전체 링크 데이터 수집
-n = int(input("Enter number of pages: "))
-yes24_page_links = []
-details = []
-for cnt in range(n):
-    print(f'\n{cnt}번째 페이지 수집\n')
-    url = f'https://www.yes24.com/Product/Category/BestSeller?categoryNumber=001&pageNumber={cnt}&pageSize=24'
-    browser.get(url)
-    datas = browser.find_elements(By.CLASS_NAME, 'gd_name')
-    for data in datas:
-        yes24_page_links.append(data.get_attribute('href'))
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from sqlalchemy import create_engine, MetaData
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-for link in yes24_page_links:
-    # 상세 페이지로 이동
-    browser.get(link)
-    title = author = publisher = date = rate = review_amount = sales_index = price = ranking = ranking_weeks = None
-    # 상세 데이터 가져오기
+def create_connection():
     try:
-        title = browser.find_element(By.CLASS_NAME, 'gd_name').text
-        author = browser.find_element(By.CLASS_NAME, 'gd_auth').text
-        publisher = browser.find_element(By.CLASS_NAME, 'gd_pub').text
-        date = browser.find_element(By.CLASS_NAME, 'gd_date').text
-        rate = browser.find_element(By.CLASS_NAME, 'gd_rating').find_element(By.TAG_NAME, 'a').find_element(By.TAG_NAME, 'em').text
-        review_amount = browser.find_element(By.CLASS_NAME, 'gd_reviewCount').text
-        sales_index = browser.find_element(By.CLASS_NAME, 'gd_sellNum').text
-        price = browser.find_element(By.CLASS_NAME, 'nor_price').text
-        ranking = browser.find_element(By.CLASS_NAME, 'gd_best ').find_element(By.TAG_NAME, 'a').text
-        ranking_weeks = browser.find_element(By.CLASS_NAME, 'gd_best').find_element(By.TAG_NAME, 'em').text
-    except NoSuchElementException:
-        pass
+        engine = create_engine('mysql+pymysql://root:123123@localhost:3306/yes24?charset=utf8mb4', echo=True)
+        return engine
+    except Exception as e:
+        print(f"ERROR: {e}")
 
-    details.append((title, author, publisher, date, rate, review_amount, sales_index, price, ranking, ranking_weeks))
 
-print(details)
+def get_table_metadata(engine):
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    return metadata.tables['books']
+
+
+def scrape_page_links(browser, url):
+    browser.get(url)
+    return [link.get_attribute('href') for link in browser.find_elements(By.CLASS_NAME, 'gd_name')]
+
+
+def scrape_book_details(browser, link):
+    try:
+        browser.get(link)
+        try:
+            title = browser.find_element(By.CLASS_NAME, 'gd_name').text
+        except NoSuchElementException:
+            title = None
+        except IndexError:
+            title = None
+
+        try:
+            author = browser.find_element(By.CLASS_NAME, 'gd_auth').text
+        except NoSuchElementException:
+            author = None
+        except IndexError:
+            author = None
+
+        try:
+            publisher = browser.find_element(By.CLASS_NAME, 'gd_pub').text
+        except NoSuchElementException:
+            publisher = None
+        except IndexError:
+            publisher = None
+
+        try:
+            date = browser.find_element(By.CLASS_NAME, 'gd_date').text
+            if date:
+                date_obj = datetime.strptime(date, '%Y년 %m월 %d일')
+                date = date_obj.strftime('%Y-%m-%d')
+        except NoSuchElementException:
+            date = None
+        except IndexError:
+            date = None
+
+        try:
+            rating = browser.find_element(By.CLASS_NAME, 'gd_rating').find_element(By.CLASS_NAME, 'yes_b').text
+        except NoSuchElementException:
+            rating = None
+        except IndexError:
+            rating = None
+
+        try:
+            review_amount = browser.find_element(By.CLASS_NAME, 'gd_reviewCount').find_element(By.CLASS_NAME,
+                                                                                               'txC_blue').text
+            temps = review_amount.split(',')
+            review_temp = ""
+            for temp in temps:
+                review_temp += temp
+            review_amount = review_temp
+        except NoSuchElementException:
+            review_amount = None
+        except IndexError:
+            review_amount = None
+
+        try:
+            sales_index = browser.find_element(By.CLASS_NAME, 'gd_sellNum').text.split(" ")[2]
+            temps = sales_index.split(",")
+            sales_temp = ""
+            for temp in temps:
+                sales_temp += temp
+            sales_index = sales_temp
+        except NoSuchElementException:
+            sales_index = None
+        except IndexError:
+            sales_index = None
+
+        try:
+            price = browser.find_element(By.CLASS_NAME, 'yes_m').text[:-1]
+            temps = price.split(",")
+            int_price = ""
+            for temp in temps:
+                int_price += temp
+            price = int_price
+        except NoSuchElementException:
+            price = None
+        except IndexError:
+            price = None
+
+        try:
+            ranking = browser.find_element(By.CLASS_NAME, 'gd_best ').text.split(" | ")[0].split(" ")[-1][:-1]
+        except NoSuchElementException:
+            ranking = None
+        except IndexError:
+            ranking = None
+
+        try:
+            ranking_weeks = browser.find_element(By.CLASS_NAME, 'gd_best').text.split(" | ")[1].split(" ")[-1][:-1]
+        except NoSuchElementException:
+            ranking_weeks = None
+        except IndexError:
+            ranking_weeks = None
+
+        return title, author, publisher, date, rating, review_amount, sales_index, price, ranking, ranking_weeks
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+
+def insert_details(engine, table, details):
+    try:
+        with engine.connect() as connection:
+            for detail in details:
+                connection.execute(table.insert(), {
+                    "title": detail[0],
+                    "author": detail[1],
+                    "publisher": detail[2],
+                    "publishing": detail[3],
+                    "rating": detail[4],
+                    "review": detail[5],
+                    "sales": detail[6],
+                    "price": detail[7],
+                    "ranking": detail[8],
+                    "ranking_weeks": detail[9]
+                })
+            connection.commit()
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+
+if __name__ == '__main__':
+    engine = create_connection()
+    table = get_table_metadata(engine)
+    ChromeDriverManager().install()
+    browser = webdriver.Chrome()
+
+    n = int(input("Enter number of pages: "))
+    details = []
+
+    for cnt in range(n):
+        print(f'\nScraping page {cnt + 1}\n')
+        url = f'https://www.yes24.com/Product/Category/BestSeller?categoryNumber=001&pageNumber={cnt + 1}&pageSize=24'
+        page_links = scrape_page_links(browser, url)
+        for link in page_links:
+            book_details = scrape_book_details(browser, link)
+            if book_details is not None:
+                details.append(book_details)
+            print(book_details)
+
+    browser.quit()
+
+    insert_details(engine, table, details)

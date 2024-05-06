d63319d68b40092014dd2b2b326d6aac33ad5b2a
Scanf-s <calzone0404@gmail.com>
2024-05-06 18:34:14 +0900

    asdf

--- Study_Flask/flask_admin_test/forms/UserCreateForm.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields.simple import StringField
-from wtforms.validators import DataRequired
-
-
-class UserCreateForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])

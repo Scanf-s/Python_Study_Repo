a1efa872da90eb1c6d60392c8d91f0ba8f82f8bf
Scanf-s <calzone0404@gmail.com>
2024-05-06 21:04:44 +0900

    Dev login endpoint

--- Project_SIMRITEST/app.py
+++ Project_SIMRITEST/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config.db import init_db, db
-from views import main_views, question_views
-from models.model_definitions import UserModel, AdminModel, AnswerModel, QuestionModel
-from flask_login import LoginManager, login_required, current_user, login_user, logout_user
-from datetime import datetime
+from models.model_definitions import AdminModel
+from views import main_views, question_views, admin_views
 
 app = Flask(__name__)
 
@@ -16,66 +15,21 @@ app.config['FLASK_ADMIN_SWATCH'] = 'cyborg'
 init_db(app)
 migrate = Migrate(app, db)
 
-# Initialize Flask-Admin lib
+
+# Register blueprints
+app.register_blueprint(main_views.main_blp)
+app.register_blueprint(question_views.question_blp)
+app.register_blueprint(admin_views.admin_blp)
+
 
 # Initialize Flask-Login lib
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'admin.login'
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return AdminModel.query.get(user_id)
 
-
-@app.route('/register', methods=["GET", "POST"])
-def register():
-    # If the user made a POST request, create a new user
-    if request.method == "POST":
-        user = AdminModel(
-            username=request.form.get("username"),
-            password=request.form.get("password"),
-            is_admin=True,
-            created_at=datetime.now()
-        )
-        db.session.add(user)
-        db.session.commit()
-
-        # Once user account created, redirect them
-        # to login route
-        return redirect(url_for("login"))
-    # Renders sign_up template if user made a GET request
-    return render_template("sign_up.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    # If a post request was made, find the user by
-    # filtering for the username
-    if request.method == "POST":
-        user = AdminModel.query.filter_by(username=request.form.get("username")).first()
-        # Check if the password entered is the
-        # same as the user's password
-        if user.password == request.form.get("password"):
-            # Use the login_user method to log in the user
-            login_user(user)
-            return redirect(url_for("home"))
-        # Redirect the user back to the home
-        # (we'll create the home route in a moment)
-    return render_template("login.html")
-
-
-@app.route("/")
-def home():
-    # Render home.html on "/" route
-    return render_template("home.html")
-
-
-
-# Register blueprints
-app.register_blueprint(main_views.main_blp)
-app.register_blueprint(question_views.question_blp)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Project_SIMRITEST/views/admin_views.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from flask import request, redirect, url_for, render_template, Blueprint, flash
+from flask_login import LoginManager, login_user, logout_user
+from app import db
+from models.model_definitions import AdminModel
+
+admin_blp = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin_blp.route('/register', methods=["GET", "POST"])
+def register():
+    # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = AdminModel(
+            username=request.form.get("username"),
+            password=request.form.get("password"),
+            is_admin=True,
+            created_at=datetime.now()
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        # Once user account created, redirect them
+        # to login route
+        return redirect(url_for("admin.login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("register.html")
+
+
+@admin_blp.route("/login", methods=["GET", "POST"])
+def login():
+    # If a post request was made, find the user by
+    # filtering for the username
+    try:
+        if request.method == "POST":
+            user = AdminModel.query.filter_by(username=request.form.get("username")).first()
+            # Check if the password entered is the
+            # same as the user's password
+            if user.password == request.form.get("password"):
+                # Use the login_user method to log in the user
+                login_user(user)
+                return redirect(url_for("admin.home"))
+    except AttributeError:
+        flash(f"username : {request.form.get("username")} not exist", "error")
+        return redirect(url_for("admin.login"))
+    except Exception as e:
+        flash("Error : {}".format(e), "error")
+        return redirect(url_for("admin.login"))
+    return render_template("login.html")
+
+
+@admin_blp.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    return redirect(url_for("admin.home"))
+
+
+
+@admin_blp.route("/")
+def home():
+    return render_template("home.html")

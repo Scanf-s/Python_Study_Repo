3fcac80cee7a4cacf7bf5882c83f2be5428f9ff9
Scanf-s <calzone0404@gmail.com>
2024-05-06 18:33:33 +0900

    asdf

--- Project_SIMRITEST/app.py
+++ Project_SIMRITEST/app.py
@@ -1,80 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_admin import Admin, AdminIndexView
-from flask_admin.contrib.sqla import ModelView
+from flask import Flask
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from config.db import init_db, db
+from views import main_views, question_views
+from models.model_definitions import UserModel, AdminModel, AnswerModel, QuestionModel
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'temp'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12341234@localhost/simritest'
+
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123123@127.0.0.1/simritest'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['FLASK_ADMIN_SWATCH'] = 'cyborg'
+
+init_db(app)
 migrate = Migrate(app, db)
 
-from app import db
-from sqlalchemy import Integer, String
+# Initialize Flask-Admin lib
 
-
-class User(db.Model, UserMixin):
-    __tablename__ = "users"
-
-    id = db.Column(Integer, primary_key=True)
-    name = db.Column(String(50), nullable=False)
-    email = db.Column(String(200), nullable=False, unique=True)
-    password = db.Column(String(100), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
-
-
-class MyModelView(ModelView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.is_admin  # `is_admin`은 admin 여부를 확인하는 속성
-
-
-class MyAdminIndexView(AdminIndexView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.is_admin
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-# Flask-Login setup
+# Initialize Flask-Login lib
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'admin.login'
 
 
 # User loader callback
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return AdminModel.query.get(int(user_id))
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        user = User.query.filter_by(email=request.form['email']).first()
-        if user and user.password == request.form['password']:  # 여기서는 해싱된 패스워드 비교로 변경해야 함
-            login_user(user)
-            return redirect(url_for('admin.index'))
-        else:
-            return 'Invalid username or password'
-    return render_template('login.html')
-
-
-admin = Admin(app, name='admin', template_mode='bootstrap3', index_view=MyAdminIndexView())
-admin.add_view(ModelView(User, db.session))
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
+# Register blueprints
+app.register_blueprint(main_views.main_blp)
+app.register_blueprint(question_views.question_blp)
 
 if __name__ == '__main__':
     app.run(debug=True)

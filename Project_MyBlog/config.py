6b550adcf34fc0db10328a89296d4a5c79f702df
Sullung <105439069+Scanf-s@users.noreply.github.com>
2024-05-13 13:22:30 +0900

    Delete Project_MyBlog directory

--- Project_MyBlog/config.py
+++
@@ -1,8 +0,0 @@
-# 데이터베이스 설정 파일
-from flask_sqlalchemy import SQLAlchemy
-
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123123@localhost/blog'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-database = SQLAlchemy()
--- Project_MyBlog/readme.md
+++
@@ -1,3 +0,0 @@
-# Flask를 이용한 간단한 Post 기능 생성
-
-#### templates/posts.html는 ChatGPT로 생성했습니다.

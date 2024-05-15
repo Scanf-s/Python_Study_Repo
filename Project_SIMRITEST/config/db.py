281ac8f0d4c75c470ad7646219e6e884a58ffcf0
Sullung <sullung2yo@gmail.com>
2024-05-16 00:04:06 +0900

    Delete project (Check new repository)

--- Project_SIMRITEST/config/db.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-def init_db(app):
-    db.init_app(app)
--- Project_SIMRITEST/forms/QuestionForm.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, BooleanField, validators
-
-
-class QuestionForm(FlaskForm):
-    content = StringField('질문 입력', validators=[validators.DataRequired()])
-    order_num = IntegerField('질문 번호', validators=[validators.DataRequired()])
-    is_active = BooleanField('활성화')
-    submit = SubmitField('추가')
